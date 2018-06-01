2eb2fddee6456c2c665f2e14ea9b4f3f56f670d7
unknown <adam9312@comcast.net>
2018-05-31 17:15:01 -0700

    mongodb-scraper

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(methodOverride('_method'));
 
 // Database configuration with mongoose
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 var db = mongoose.connection;
